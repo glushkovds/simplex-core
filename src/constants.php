97bb6195f4b82174dca00171cdd8ed4f75ad3af0
Denis Glushkov <amkarovec@gmail.com>
2020-11-01 17:28:11 +0500

    constants

---
+++ src/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SF_LOCATION_SITE', 'site');
+define('SF_LOCATION_CLI', 'cli');
+define('SF_LOCATION_ADMIN', 'admin');
+define('SF_LOCATION_API', 'api');
+
+define('SF_CORE_ROOT_PATH', __DIR__);
