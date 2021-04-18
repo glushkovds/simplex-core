e2c44e2647d1fda8641d624100861b38c69f2679
Denis Glushkov <amkarovec@gmail.com>
2021-04-18 13:04:40 +0500

    Buffer::delete method

--- src/Core/Buffer.php
+++ src/Core/Buffer.php
@@ -8,12 +8,27 @@ class Buffer
 
     protected static $data = [];
 
+    /**
+     * @param string $key
+     * @param callable $callback
+     * @return mixed
+     */
     public static function getOrSet($key, callable $callback)
     {
         if (!array_key_exists($key, static::$data)) {
             static::$data[$key] = $callback();
         }
         return static::$data[$key];
+    }
+
+    /**
+     * @param string $key
+     */
+    public static function delete($key)
+    {
+        if (array_key_exists($key, static::$data)) {
+            unset(static::$data[$key]);
+        }
     }
 
 }
